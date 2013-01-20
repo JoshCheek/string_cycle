5bc0e7ba54e3ec17a0fc591916254536c09b081a
Josh Cheek <josh.cheek@gmail.com>
2013-01-20 14:12:49 -0600

    Make a bit more progress

--- ext/string_cycle/string_cycle.c
+++ ext/string_cycle/string_cycle.c
@@ -1,5 +1,6 @@
 #include <ruby.h>
 
+// change these to CamelCase
 typedef struct linked_list_node {
   char                      c;
   struct linked_list_node*  next;
@@ -10,19 +11,6 @@ typedef struct {
   linked_list_node*  tail;
   int                size;
 } linked_list;
-
-
-// struct dbmdata {
-//     int  di_size;
-//     DBM *di_dbm;
-// };
-//
-// obj = Data_Make_Struct(klass, struct dbmdata, 0, free_dbm, dbmp);
-// #define GetDBM(obj, dbmp) {\
-//     Data_Get_Struct(obj, struct dbmdata, dbmp);\
-//     if (dbmp->di_dbm == 0) closed_dbm();\
-// }
-
 
 linked_list_node* new_linked_list_node(char c, linked_list_node* next_node) {
   linked_list_node* new_node = (linked_list_node*) malloc(sizeof(linked_list_node));
@@ -61,17 +49,39 @@ void append_char(linked_list* list, char c) {
   ++(list->size);
 }
 
-//    CDJukebox *jukebox;
-//    VALUE obj;
-//    // Vendor library creates the Jukebox
-//    jukebox = new_jukebox();
-//    // then we wrap it inside a Ruby CDPlayer object
-//    obj = Data_Wrap_Struct(klass, 0, cd_free, jukebox);
+/*
+static VALUE cd_alloc(VALUE klass) {
+  CDJukebox *jukebox;
+  VALUE obj;
+  // Vendor library creates the Jukebox
+  jukebox = new_jukebox();
+  // then we wrap it inside a Ruby CDPlayer object
+  obj = Data_Wrap_Struct(klass, 0, cd_free, jukebox);
+  return obj;
+}
+void Init_CDPlayer() {
+  cCDPlayer = rb_define_class("CDPlayer", rb_cObject);
+  rb_define_alloc_func(cCDPlayer, cd_alloc);
+  // ...
+}
+static VALUE cd_initialize(VALUE self, VALUE unit) {
+  int unit_id;
+  CDJukebox *jb;
+  Data_Get_Struct(self, CDJukebox, jb);
+  unit_id = NUM2INT(unit);
+  assign_jukebox(jb, unit_id);
+  return self;
+}
+*/
+
+static VALUE string_cycle_alloc(VALUE klass) {
+  return Data_Wrap_Struct(klass, 0, free_linked_list, new_linked_list());
+}
 
 // need to convert to the data structure
-VALUE string_cycle_initialize(VALUE self, VALUE string_to_cycle) {
-  /* linked_list* list     = new_linked_list(); */
-  /* VALUE        list_obj = Data_Wrap_Struct( */
+static VALUE string_cycle_initialize(VALUE self, VALUE string_to_cycle) {
+  linked_list* list;
+  Data_Get_Struct(self, linked_list, list);
   return rb_iv_set(self, "@list", string_to_cycle);
 }
 
@@ -82,6 +92,7 @@ VALUE string_cycle_to_s(VALUE self) {
 
 void Init_string_cycle() {
   VALUE StringCycle = rb_define_class("StringCycle", rb_cObject);
+  rb_define_alloc_func(StringCycle, string_cycle_alloc);
   rb_define_method(StringCycle, "initialize", string_cycle_initialize, 1);
   rb_define_method(StringCycle, "to_s",       string_cycle_to_s,       0);
 }
