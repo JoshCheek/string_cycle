7de0f9e2b12812c46a761fe1628bd815b6be8faa
Josh Cheek <josh.cheek@gmail.com>
2013-01-20 14:19:27 -0600

    Renamings (structs to CamelCase, methods prepended with sc_)

--- ext/string_cycle/string_cycle.c
+++ ext/string_cycle/string_cycle.c
@@ -1,46 +1,45 @@
 #include <ruby.h>
 
-// change these to CamelCase
 typedef struct linked_list_node {
   char                      c;
   struct linked_list_node*  next;
-} linked_list_node;
+} SCLinkedListNode;
 
 typedef struct {
-  linked_list_node*  head;
-  linked_list_node*  tail;
+  SCLinkedListNode*  head;
+  SCLinkedListNode*  tail;
   int                size;
-} linked_list;
+} SCLinkedList;
 
-linked_list_node* new_linked_list_node(char c, linked_list_node* next_node) {
-  linked_list_node* new_node = (linked_list_node*) malloc(sizeof(linked_list_node));
-  new_node->c                = c;
-  new_node->next             = next_node;
+SCLinkedListNode* sc_new_linked_list_node(char c, SCLinkedListNode* next_node) {
+  SCLinkedListNode* new_node = (SCLinkedListNode*) malloc(sizeof(SCLinkedListNode));
+  new_node->c               = c;
+  new_node->next            = next_node;
   return new_node;
 }
 
-linked_list* new_linked_list() {
-  linked_list* new_list = (linked_list*) malloc(sizeof(linked_list));
-  new_list->head        = NULL;
-  new_list->tail        = NULL;
-  new_list->size        = 0;
+SCLinkedList* sc_new_linked_list() {
+  SCLinkedList* new_list = (SCLinkedList*) malloc(sizeof(SCLinkedList));
+  new_list->head         = NULL;
+  new_list->tail         = NULL;
+  new_list->size         = 0;
   return new_list;
 }
 
-void free_linked_list(void *p) {
-  linked_list*      list         = p;
-  linked_list_node* prev_node    = NULL;
-  linked_list_node* current_node = list->head;
+void sc_free_linked_list(void *p) {
+  SCLinkedList*     list         = p;
+  SCLinkedListNode* prev_node    = NULL;
+  SCLinkedListNode* current_node = list->head;
   while(current_node) {
-    prev_node = current_node;
+    prev_node    = current_node;
     current_node = current_node->next;
     free(prev_node);
   }
   free(list);
 }
 
-void append_char(linked_list* list, char c) {
-  linked_list_node* node = new_linked_list_node(c, NULL);
+void append_char(SCLinkedList* list, char c) {
+  SCLinkedListNode* node = sc_new_linked_list_node(c, NULL);
   if(list->size == 0)
     list->head = node;
   else
@@ -59,11 +58,13 @@ static VALUE cd_alloc(VALUE klass) {
   obj = Data_Wrap_Struct(klass, 0, cd_free, jukebox);
   return obj;
 }
+
 void Init_CDPlayer() {
   cCDPlayer = rb_define_class("CDPlayer", rb_cObject);
   rb_define_alloc_func(cCDPlayer, cd_alloc);
   // ...
 }
+
 static VALUE cd_initialize(VALUE self, VALUE unit) {
   int unit_id;
   CDJukebox *jb;
@@ -74,25 +75,30 @@ static VALUE cd_initialize(VALUE self, VALUE unit) {
 }
 */
 
-static VALUE string_cycle_alloc(VALUE klass) {
-  return Data_Wrap_Struct(klass, 0, free_linked_list, new_linked_list());
+static VALUE sc_alloc(VALUE klass) {
+  return Data_Wrap_Struct(klass, 0, sc_free_linked_list, sc_new_linked_list());
 }
 
 // need to convert to the data structure
-static VALUE string_cycle_initialize(VALUE self, VALUE string_to_cycle) {
-  linked_list* list;
-  Data_Get_Struct(self, linked_list, list);
+static VALUE sc_initialize(VALUE self, VALUE string_to_cycle) {
+  // figure out how to do: string_to_cycle.each_char { |c| append_char(list, c) }
+  SCLinkedList* list;
+  Data_Get_Struct(self, SCLinkedList, list);
+
+  // just storing the list to show that this code actually does something
+  // after figuring out how to put it into the list data structure, though,
+  // this should be removed
   return rb_iv_set(self, "@list", string_to_cycle);
 }
 
 // need to convert back to Ruby string
-VALUE string_cycle_to_s(VALUE self) {
+VALUE sc_to_s(VALUE self) {
   return rb_iv_get(self, "@list");
 }
 
 void Init_string_cycle() {
   VALUE StringCycle = rb_define_class("StringCycle", rb_cObject);
-  rb_define_alloc_func(StringCycle, string_cycle_alloc);
-  rb_define_method(StringCycle, "initialize", string_cycle_initialize, 1);
-  rb_define_method(StringCycle, "to_s",       string_cycle_to_s,       0);
+  rb_define_alloc_func(StringCycle, sc_alloc);
+  rb_define_method(StringCycle, "initialize", sc_initialize, 1);
+  rb_define_method(StringCycle, "to_s",       sc_to_s,       0);
 }
